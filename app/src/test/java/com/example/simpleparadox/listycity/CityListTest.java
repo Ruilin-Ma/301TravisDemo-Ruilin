66061fc20d1c2e64151b72252ab0b2a4ba264a7b
Ruilin-Ma <71634749+Ruilin-Ma@users.noreply.github.com>
2021-03-21 21:10:58 +0800

    new test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -1,5 +1,6 @@
 package com.example.simpleparadox.listycity;
 
+import org.junit.Test;
 import org.junit.jupiter.api.Test;
 
 
@@ -27,5 +28,47 @@ class CityListTest {
 
         assertEquals(2, cityList.countCities());
         assertTrue(cityList.hasCity(new City("Regina", "Saskatchewan")));
+    }
+
+    @Test
+    void testAddExcepetion() {
+        CityList cityList = mockCityList();
+
+        City city = new City("Yellowknife", "Northwest Territories");
+        cityList.add(city);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            cityList.add(city);
+        });
+    }
+
+    @Test
+    void testHasCity() {
+        CityList cityList = mockCityList();
+
+        assertTrue(cityList.hasCity(mockCity()));
+    }
+
+    @Test
+    void testGetCities() {
+        CityList cityList = mockCityList();
+
+        assertEquals(1,mockCity().compareTo(cityList.getCities().get(0)));
+        City city = new City("Charlottetown", "Prience Edward Island");
+        citylist.add(city);
+
+        assertEquals(0, city.compareTo(cityList.getCities().get(0)));
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(0)));
+
+    }
+    
+    @Test
+    void testDeleteCity(){
+        CityList cityList = mockCityList();
+        City city = new City("Victoria", "British Columbia");
+        citylist.add(city);
+        assertEquals(2, cityList.countCities());
+        cityList.delete(mockCity());
+        assertEquals(1,cityList.countCities());    
     }
 }
